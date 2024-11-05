f6ad72c768e132405639d68897b78d998e95151c
Natalia Namie <namiekomesso@gmail.com>
2024-11-05 09:36:48 -0300

    advanced functions

---
+++ scripts/advanced-functions-todo-list.js
@@ -0,0 +1,75 @@
+/*
+Steps (Algorithm)
+1. Create array to store todos
+2. When we click "Add",
+3. Get text from textbox
+4. Add it to array
+5. console.log() the array
+*/
+
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+}, {
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+
+    let todoListHTML = '';
+
+    todoList.forEach(function(todoObject, index) {
+        const { name, dueDate } = todoObject;
+        const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button onclick="
+                todoList.splice(${index}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        `;
+        todoListHTML += html;
+    });
+
+    /* Substituído por forEach acima.
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        // const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject;
+        const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        `;
+        todoListHTML += html;
+    }
+    */
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        // name: name,
+        // dueDate: dueDate,
+        name,
+        dueDate,
+    });
+
+    inputElement.value = '';
+
+    renderTodoList();
+}
