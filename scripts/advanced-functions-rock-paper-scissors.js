74f82c385b21448c302a6bc98587dc8c0c8d56de
Natalia Namie <namiekomesso@gmail.com>
2024-11-05 11:27:49 -0300

    advanced funtions part 2

--- scripts/advanced-functions-rock-paper-scissors.js
+++ scripts/advanced-functions-rock-paper-scissors.js
@@ -19,7 +19,11 @@ if (!score) {
 let isAutoPlay = false;
 let intervalId;
 
-function autoPlay() {
+// const autoPlay = () => {
+
+// };
+
+function autoPlay() { // prefer this: 1. easy to read /2. hoisting
     if (!isAutoPlay) {
         intervalId = setInterval(() => {
             const playerMove = pickComputerMove();
@@ -34,6 +38,28 @@ function autoPlay() {
     
 
 }
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+    playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+    playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    }
+});
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
